6c0855d1124452d29019e784c4288d9aef35f1dd
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-09-09 09:33:20 -0700

    help fixed bugs

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - print text to stderr
  *
- *  @return 1
+ * Return: 1;
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
