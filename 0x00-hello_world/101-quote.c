d6d1742ef7f514120866ad7b2c0773b957467af3
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-09-09 08:13:49 -0700

    add c program to print to standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,12 @@
 
 /**
  * main - print text to stderr
- * @Return 1
+ *
+ *  @return 1
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora\
+		       	Korpar, 2015-10-19\n");
 	return (1);
 }
