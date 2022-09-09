53dc3180614b9dadff9461cf6df45ce5604284a5
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-09-08 19:58:47 -0700

    add c_file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
  * main - print out line to stdout stream
+ *
+ * Return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
