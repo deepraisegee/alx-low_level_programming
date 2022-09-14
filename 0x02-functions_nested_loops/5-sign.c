b7eeae167fdebcdf3f47375c69c3b1586032ed37
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-09-13 19:04:29 -0700

    add a c file

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * print_sign - prints the sign of a number
+  *
+  * @n: an interger
+  *
+  * Return: 1 if n is +ve, 0 if it's zero or -1 if it's -ve
+  */
+int print_sign(int n)
+{
+	int ascii_plus = 43;
+	int ascii_minus = 45;
+	int ascii_zero = 48;
+
+	if (n > 0)
+	{
+		putchar(ascii_plus);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar(ascii_zero);
+		return (0);
+	}
+	else
+	{
+		putchar(ascii_minus);
+		return (-1);
+	}
+}
