1d59aea556807e7c47c2a308dffcdb6208839938
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-09-13 18:13:21 -0700

    add a c file

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * _islower - checks if an alphabet is lower or not
+  *
+  * Return: 1 if it's lower, otherwise 0
+  */
+int _islower(int c)
+{
+	ascii_a = 97;
+	ascii_z = 122;
+
+	if ((c >= ascii_a) && (c <= ascii_c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
