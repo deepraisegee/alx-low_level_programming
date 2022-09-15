e0ca40e218c09ec60d9e55db056efd877e7c6646
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-09-15 16:53:52 -0700

    message

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * _isupper - checks for uppercase character
+  * @c: an integer
+  *
+  * Return: 1 or 0
+  */
+int _isupper(int c)
+{
+	int ascii_A = 65;
+	int ascii_Z = 90;
+
+	if ((n >= ascii_A) && (n <= ascii_Z))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
