14acaf841b56ee2e8191cc4744bd6f2cf70e4cdf
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-10-02 16:21:13 -0700

    c file to print file name added

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
   * main - prints the file name
@@ -7,7 +7,9 @@
   *
   * Return: Always 0
   */
-int main(int argc, char *argv[])
+int main(int argc __attribute__ ((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
+
+	return (0);
 }
