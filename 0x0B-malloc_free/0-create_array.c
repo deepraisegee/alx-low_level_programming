e1b09cf637745db97b81780a811ee4796859af0d
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-10-05 12:18:06 -0700

    add a malloc c file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * create_array - creates an array of chars,
+  * and initializes it with a specific char.
+  * @size: unsigned integer
+  * @c: character
+  *
+  * Return: NULL is size is 0 otherwise a pointer
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *string = malloc(sizeof(c) * (size + 1));
+
+	if (size == 0)
+		return (NULL);
+
+	if (!string)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		string[i] = c;
+	}
+
+	string[size] = '\n';
+
+	return (string);
+}
