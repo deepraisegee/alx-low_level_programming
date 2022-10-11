ca18796fd49d2645503eae9f11ff789fbc808ee2
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-10-10 23:31:03 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,18 +7,25 @@
 typedef struct dog dog_t;
 
 /**
-  * struct dog - a structure for dog
-  * @name: pointer
-  * @age: integer
-  * @owner: pointer
-  *
-  * Description: structure for dog
-  */
+ *struct dog  - description of dog
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: dog's owner
+ */
+
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
-}
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
