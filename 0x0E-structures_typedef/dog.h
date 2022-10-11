9eabec63b827649cba4fff146a7b7b1d73b8c9a2
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-10-10 23:22:56 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+  * struct dog - a structure for dog
+  * @name: pointer
+  * @age: integer
+  * @owner: pointer
+  *
+  * Description: structure for dog
+  */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
