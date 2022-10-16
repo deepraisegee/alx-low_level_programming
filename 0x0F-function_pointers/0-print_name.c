0099b2b097282f23d33b527e1da6ffdaf3eadde5
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-10-15 17:07:57 -0700

    function poniter

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+  * print_name - prints a name.
+  * @name: char pointer
+  * @f: function pointer
+  *
+  * Return: nothing
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
