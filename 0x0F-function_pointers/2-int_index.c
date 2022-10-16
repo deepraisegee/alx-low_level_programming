801c04cfc4ed7012b7d699d09f08c937fa063348
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2022-10-15 17:51:34 -0700

    function

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+  * int_index - searches for an integer.
+  * @array: int pointer
+  * @size: int
+  * @cmp: function pointer
+  *
+  * Return: int
+  */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, x;
+
+	if (!array || !cmp)
+		exit(EXIT_FAILURE);
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		x = cmp(array[i]);
+		if (x == 1)
+			return (i);
+	}
+
+	return (-1);
+}
