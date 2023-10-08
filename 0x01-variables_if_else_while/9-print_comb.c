c80f8c23e575413e383f2fdf69af73eb1bebf1af
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-08 21:26:00 +0000

    add c code

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+  * main - print all combination of number
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+
+		if (n <= '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
