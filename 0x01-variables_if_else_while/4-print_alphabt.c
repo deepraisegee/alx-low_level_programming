29812fc19b62c947b39e134221898e0468737b3a
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-08 19:50:02 +0000

    add c code

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+  * main - print lower alphabet except 'q'
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		if (alp == 'q')
+			continue;
+		putchar(alp);
+	}
+	putchar('\n');
+
+	return (0);
+}
