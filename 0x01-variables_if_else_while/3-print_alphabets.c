affcf88e6b45917a147ae374abbe22c2f7de6be0
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-08 19:42:05 +0000

    add c code

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - print all aphabets
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char alp = 'a';
+
+	while (apl != 'Z' + 1)
+	{
+		putchar(alp);
+		alp++;
+		if (alp == 'z')
+		{
+			alp = 'A';
+		}
+	}
+	putchar('\n');
+}
