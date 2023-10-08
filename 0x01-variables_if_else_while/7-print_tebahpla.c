ffefd8f3fbeefe0598f7201a2a241a130d64a087
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-08 20:06:28 +0000

    add c code

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - print alphabet in reverse order
+  *
+  * Return: 0
+  */
+int main(void)
+{
+	char alp = 'z';
+
+	while (alp >= 'a')
+	{
+		putchar(alp);
+		alp--;
+	}
+	putchar('\n');
+}
