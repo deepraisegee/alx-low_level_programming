85bee87593fdbbb5b136dff048c59d5b62ca298e
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-11 01:04:58 +0000

    add c code

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_alphabet - prints the alphabet in lowercase.
+  *
+  * Return: Nothing
+  */
+void print_alphabet(void)
+{
+	char alp = 'a';
+
+	while (alp <= 'z')
+	{
+		_putchar(alp);
+		alp++;
+	}
+	_putchar('\n');
+
+	return (0);
+}
