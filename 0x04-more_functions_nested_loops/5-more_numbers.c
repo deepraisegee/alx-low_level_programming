4cc2d779ef22f5e5a6d237ba5edd237ff1046926
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-13 22:18:40 +0000

    add c code

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * more_numbers - prints 10 times the numbers, from 0 to 14.
+  *
+  * Return: void.
+  */
+void more_numbers(void)
+{
+	int t = 0;
+
+	while (t < 10)
+	{
+		for (i = '0'; i <= '9'; i++)
+		{
+			_putchar(i);
+		}
+
+
+	}
+
+	d = '0';
+}
