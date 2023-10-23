7064605ec28acc376df623f0df25fd3ea3c0011d
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-23 20:49:02 +0000

    add 2-strchr.c file

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,11 +9,12 @@
   */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	int i;
+
+	for (i = 0; i != '\0'; i++)
 	{
-		if (*s == c)
-			return (s);
-		s++;
+		if (s[i] == c)
+			return (&(s[i]));
 	}
 
 	return (NULL);
