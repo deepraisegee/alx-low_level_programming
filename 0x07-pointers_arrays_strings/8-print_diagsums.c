1bc719d70cf45b202b20dd6ad9c2f39947cea940
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-24 00:39:28 +0000

    yet another

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,5 +10,15 @@
   */
 void print_diagsums(int *a, int size)
 {
-	
+	int i, j;
+	int s1 = 0;
+	int s2 = 0;
+
+	for (i = 0; i < size; ++i)
+		s1 += (a + i * size)[i];
+
+	for (j = 0; j < size; ++j)
+		s2 += (a + j size)[--i]
+
+	printf("%d, %d\n", s1, s2);
 }
