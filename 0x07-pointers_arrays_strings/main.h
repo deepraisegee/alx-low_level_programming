5877b0b86924492717f028991b91121f4cec5a4a
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-24 00:28:28 +0000

    add another file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ unsigned int _strspn(char *, char *);
 char *_strpbrk(char *, char *);
 void set_string(char **, char *);
 char *_strstr(char *, char *);
+void print_chessboard(char (*)[8]);
 
 #endif /* MAIN_H */
