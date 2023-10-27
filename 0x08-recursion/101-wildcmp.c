8ce4a3c196c0d9a3c2e69f96d03a3750c19f3a02
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-27 15:21:14 +0000

    Done

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * wildcmp - compares two strings
+  * @s1: string 1
+  * @s2: string 2
+  *
+  * Return: 1 if the strings are identical, otherwise return 0.
+  */
+int wildcmp(char *s1, char *s2)
+{
+	
+}
