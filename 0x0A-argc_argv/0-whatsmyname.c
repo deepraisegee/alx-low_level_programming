0b32d595924cc0496f74eaac89d52384ddf4eb55
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-10-27 21:02:48 +0000

    Add c file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * main - print the program name.
+  * @argc: number of arguments
+  * @argv: list of arguments.
+  *
+  * Return: int.
+  */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+# Command argument in C.
