0fa28fc69beae5d76db587984ef357de82f7ee65
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-11-01 02:15:35 +0000

    C file added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * create_array - creates an array of chars
+  * and initializes it with a specific char.
+  * @size: number of byte.
+  * @c: character
+  *
+  * Return: pointer to the array if created, otherwise NULL
+  */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *array = malloc(sizeof(c) * size);
+
+	if (!array)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		array[i] = c;
+
+	return (array);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+char *create_array(unsigned int, char);
+
+#endif /* MAIN_H */
