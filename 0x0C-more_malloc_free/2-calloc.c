abd0c89361d07cc856de495bac28175157f2c700
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-11-02 23:48:32 +0000

    Yet another file added

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _calloc - allocates memory for an array, using malloc.
+  * @nmemb: number of elements in the array
+  * @size: size of byte of each element
+  *
+  * Return: eturns a pointer to the allocated memory.
+  */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	int *m;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	m = malloc(sizeof(size) * nmemb);
+
+	if (m == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		m[i] = 0;
+
+	return (m);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,6 @@
 int _putchar(char);
 void *malloc_checked(unsigned int);
 char *string_nconcat(char *, char *, unsigned int);
+void *_calloc(unsigned int, unsigned int);
 
 #endif /* MAIN_H */
