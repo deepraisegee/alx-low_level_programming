26a5c3a34e991d08d1cf4546cd14df8a2ad33290
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-11-08 23:25:08 +0000

    Add a C file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <stdlib.h>
+
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
