2f4d42bdf0feb69fd0937340500612da3d17eb34
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-11-14 21:35:20 +0000

    Fixed

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * argstostr - concatenates all the arguments
+  * @ac: arg count
+  * @av: arg vector
+  *
+  * Return: pointer to the concatinated value
+  */
+char *argstostr(int ac, char **av)
+{
+	
+}
