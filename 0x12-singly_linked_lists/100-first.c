14c1e7fb5a1629393413db314c4dca1c94261c6c
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2023-11-17 11:41:27 +0000

    Add a file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * before - print message before main executes
+  *
+  * Return: void
+  */
+void before(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
