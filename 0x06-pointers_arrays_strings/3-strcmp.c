879e980af4f9a7897fc77be311334c7193866412
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-01-07 11:11:07 +0000

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,25 +1,6 @@
 #include "main.h"
 
 /**
-  * _strlen - returns the length of a string.
-  * @s: string value
-  *
-  * Return: lenght of s.
-  */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (*s != '\0')
-	{
-		s++;
-		i++;
-	}
-
-	return (i);
-}
-
-/**
   * _strcmp - compare 2 strings
   * @s1: string 1
   * @s2: string 2
@@ -28,19 +9,13 @@ int _strlen(char *s)
   */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int s1_l = _strlen(s1);
-	int s2_l = _strlen(s2);
-
-	if (s1_l < s2_l)
-		return (-1);
-	else if (s1_l > s2_l)
-		return (1);
-
-	for (i = 0; s1[i]; i++)
+	while (1)
 	{
-		if (s1[i] != s2[i])
-			return (-1);
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		if (*s1 == '\0')
+			return (0);
+		s1++;
+		s2++;
 	}
-	return (0);
 }
