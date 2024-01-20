228f84b93a89d92b6389dc9b59be86d965561549
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-01-20 01:25:45 +0000

    0x18-dynamic_libraries

---
+++ 0x18-dynamic_libraries/calc.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+  * add - add 2 numbers
+  * @a: 1st No
+  * @b: 2nd No
+  *
+  * Return: the result
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * sub - subtract 2 numbers
+  * @a: 1st No
+  * @b: 2nd No
+  *
+  * Return: the result
+  */
+int sub(int a, int b)
+{
+    return (a - b);
+}
+
+/**
+  * mul - multiply 2 numbers
+  * @a: 1st No
+  * @b: 2nd No
+  *
+  * Return: the result
+  */
+int mul(int a, int b)
+{
+    return (a * b);
+}
+
+/**
+  * div - divide 2 numbers
+  * @a: 1st No
+  * @b: 2nd No
+  *
+  * Return: the result
+  */
+int div(int a, int b)
+{
+    return (a / b);
+}
