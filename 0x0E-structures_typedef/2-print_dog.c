a04a087e60c19f8c5db08bddb1d30f57298c8209
Adesanmi Dayo PraiseGod <deepraisegee@gmail.com>
2024-03-23 15:25:56 +0000

    Done

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,8 @@ void print_dog(struct dog *d)
 		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
+	if (d->age == NULL)
+		printf("Age: (nil)\n");
 	if (d->owner == NULL)
 		printf("Owner: (nil)\n");
 
